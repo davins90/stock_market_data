ff33913dee7933730ffe0bf1a37cb0a341b74233
dani <daniele.davino@live.it>
2020-08-27 09:51:53 +0200

    tolto euro zone per provare se il resto funziona

--- first_app.py
+++ first_app.py
@@ -52,7 +52,7 @@ eur_yen = investpy.get_currency_cross_recent_data(currency_cross='EUR/JPY')
 ## Indici di mercato 
 
 df_it = investpy.get_index_recent_data(index='MSCI Italy Net EUR',country='italy') # italia
-df_eu = investpy.get_index_recent_data(index='MSCI Europe Net EUR',country='euro zone') # europa
+# df_eu = investpy.get_index_recent_data(index='MSCI Europe Net EUR',country='euro zone') # europa
 df_us = investpy.get_index_recent_data(index='MSCI US Net USD',country='united states') # usa
 df_jp = investpy.get_index_recent_data(index='MSCI Japan Net JPY',country='japan') # japan
 df_ci = investpy.get_index_recent_data(index='MSCI China Net USD',country='china') # china
@@ -66,7 +66,8 @@ df_wt = investpy.get_index_recent_data(index='Bloomberg WTI Crude Oil TR',countr
 
 ## Creo dataframe finale
 
-index = [df_it,df_eu,df_us,df_jp,df_ci,df_mt,df_em,df_ge,df_gu,df_iu,df_or,df_wt,eur_gbp,eur_usd,eur_yen]
+# index = [df_it,df_eu,df_us,df_jp,df_ci,df_mt,df_em,df_ge,df_gu,df_iu,df_or,df_wt,eur_gbp,eur_usd,eur_yen]
+index = [df_it,df_us,df_jp,df_ci,df_mt,df_em,df_ge,df_gu,df_iu,df_or,df_wt,eur_gbp,eur_usd,eur_yen]
 
 val_all = []
 for i in range(len(index)):
@@ -77,20 +78,20 @@ val_all = val_all.rename(columns={0:'perf_sett_%'})
 val_all['tipo'] = 'nome_indice_da_inserire'
 
 val_all['tipo'][0] = 'azioni_italia'
-val_all['tipo'][1] = 'azioni_europa'
-val_all['tipo'][2] = 'azioni_usa_usd'
-val_all['tipo'][3] = 'azioni_japan_yen'
-val_all['tipo'][4] = 'azioni_cina_usd'
-val_all['tipo'][5] = 'azioni_world_info_tech_usd'
-val_all['tipo'][6] = 'azioni_emergenti_usd'
-val_all['tipo'][7] = 'bond_governativi_eurozona'
-val_all['tipo'][8] = 'bond_governativi_usa'
-val_all['tipo'][9] = 'bond_corporate_usa'
-val_all['tipo'][10] = 'oro'
-val_all['tipo'][11] = 'wti'
-val_all['tipo'][12] = 'euro_sterlina'
-val_all['tipo'][13] = 'euro_dollaro'
-val_all['tipo'][14] = 'euro_yen'
+# val_all['tipo'][1] = 'azioni_europa'
+val_all['tipo'][1] = 'azioni_usa_usd'
+val_all['tipo'][2] = 'azioni_japan_yen'
+val_all['tipo'][3] = 'azioni_cina_usd'
+val_all['tipo'][4] = 'azioni_world_info_tech_usd'
+val_all['tipo'][5] = 'azioni_emergenti_usd'
+val_all['tipo'][6] = 'bond_governativi_eurozona'
+val_all['tipo'][7] = 'bond_governativi_usa'
+val_all['tipo'][8] = 'bond_corporate_usa'
+val_all['tipo'][9] = 'oro'
+val_all['tipo'][10] = 'wti'
+val_all['tipo'][11] = 'euro_sterlina'
+val_all['tipo'][12] = 'euro_dollaro'
+val_all['tipo'][13] = 'euro_yen'
 val_all = val_all[['tipo','perf_sett_%']]
 
 ## Aggancio i dati dello spread
