7ad0d421fd1bbe3c6dcfdd2ab8c73cf5027a82c9
dani <daniele.davino@live.it>
2020-08-27 13:10:09 +0200

    aggiunto tutto tranne world e eurozona nel download

--- first_app.py
+++ first_app.py
@@ -30,7 +30,7 @@ st.title('Come sono andati i mercati questa settimana?')
 st.write('Overview super generica sui mercati e link per il download del csv in fondo alla pagina.')
 
 def fun():
-    st.write('Sto scaricando i dati...')
+    st.write('Ecco i dati! :)')
     ## Dati dello Spread da Borsa Italiana
     url = 'https://borsaitaliana.teleborsa.it/pages/spread/item.aspx'
     try:
@@ -53,23 +53,23 @@ def fun():
 
     ## Indici di mercato 
 
-    # df_it = investpy.get_index_recent_data(index='MSCI Italy Net EUR',country='italy') # italia
+    df_it = investpy.get_index_recent_data(index='MSCI Italy Net EUR',country='italy') # italia
     # df_eu = investpy.get_index_recent_data(index='MSCI Europe Net EUR',country='euro zone') # europa
-    # df_us = investpy.get_index_recent_data(index='MSCI US Net USD',country='united states') # usa
-    # df_jp = investpy.get_index_recent_data(index='MSCI Japan Net JPY',country='japan') # japan
-    # df_ci = investpy.get_index_recent_data(index='MSCI China Net USD',country='china') # china
+    df_us = investpy.get_index_recent_data(index='MSCI US Net USD',country='united states') # usa
+    df_jp = investpy.get_index_recent_data(index='MSCI Japan Net JPY',country='japan') # japan
+    df_ci = investpy.get_index_recent_data(index='MSCI China Net USD',country='china') # china
     # df_mt = investpy.get_index_recent_data(index='MSCI World Info Tech Net USD',country='world') # mondo tech
     # df_em = investpy.get_index_recent_data(index='MSCI EM Net USD',country='world') # emergenti
     # df_ge = investpy.get_index_recent_data(index='TR Eurozone 10 Years Government Benchmark',country='euro zone') # governativo eurozona
-    # df_gu = investpy.get_index_recent_data(index='TR US 10 Year Government Benchmark',country='united states') # governativo usa
-    # df_iu = investpy.get_index_recent_data(index='DJ Equal Weight US Issued Corporate Bond TR',country='united states') # ig corp usa 
+    df_gu = investpy.get_index_recent_data(index='TR US 10 Year Government Benchmark',country='united states') # governativo usa
+    df_iu = investpy.get_index_recent_data(index='DJ Equal Weight US Issued Corporate Bond TR',country='united states') # ig corp usa 
     # df_or = investpy.get_index_recent_data(index='Bloomberg Gold TR',country='world') # oro 
     # df_wt = investpy.get_index_recent_data(index='Bloomberg WTI Crude Oil TR',country='world') # wti
 
     ## Creo dataframe finale
 
     # index = [df_it,df_eu,df_us,df_jp,df_ci,df_mt,df_em,df_ge,df_gu,df_iu,df_or,df_wt,eur_gbp,eur_usd,eur_yen]
-    index = [eur_gbp,eur_usd,eur_yen]
+    # index = [eur_gbp,eur_usd,eur_yen]
 
     val_all = []
     for i in range(len(index)):
@@ -95,9 +95,19 @@ def fun():
     # val_all['tipo'][13] = 'euro_dollaro'
     # val_all['tipo'][14] = 'euro_yen'
 
-    val_all['tipo'][0] = 'euro_sterlina'
-    val_all['tipo'][1] = 'euro_dollaro'
-    val_all['tipo'][2] = 'euro_yen'
+    val_all['tipo'][0] = 'azioni_italia'
+    val_all['tipo'][1] = 'azioni_usa_usd'
+    val_all['tipo'][2] = 'azioni_japan_yen'
+    val_all['tipo'][3] = 'azioni_cina_usd'
+    val_all['tipo'][4] = 'bond_governativi_usa'
+    val_all['tipo'][5] = 'bond_corporate_usa'
+    val_all['tipo'][6] = 'euro_sterlina'
+    val_all['tipo'][7] = 'euro_dollaro'
+    val_all['tipo'][8] = 'euro_yen'
+
+    # val_all['tipo'][0] = 'euro_sterlina'
+    # val_all['tipo'][1] = 'euro_dollaro'
+    # val_all['tipo'][2] = 'euro_yen'
 
     val_all = val_all[['tipo','perf_sett_%']]
 
@@ -114,5 +124,5 @@ def fun():
     st.markdown(get_table_download_link(df), unsafe_allow_html=True)
     return
 
-if st.button('clicca qua per avviare aggiornamento dei dati di mercato'):
+if st.button('Clicca qua per avviare aggiornamento dei dati'):
     fun()
