b24817aa14a22e462f2f29a0492a59b7f4389a3f
dani <daniele.davino@live.it>
2021-04-09 16:31:17 +0200

    update investpy

--- first_app.py
+++ first_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import investpy
+import investpy # versione aggiornata: per ora solo fork sull'originale, poi sistema meglio
 import time
 import base64
 
